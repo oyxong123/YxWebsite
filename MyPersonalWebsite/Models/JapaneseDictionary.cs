005f477857de34d6fc113f100b5c1c70bfee811e
yuxuan <64518301+oyxong123@users.noreply.github.com>
2023-06-05 00:34:42 +0800

    Set up basic database model

---
+++ MyPersonalWebsite/Models/JapaneseDictionary.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPersonalWebsite.Models
+{
+    public class JapaneseDictionary
+    {
+        public int Id { get; set; }
+        public string? Phrase { get; set; }
+        public string? Meaning { get; set; }
+    }
+}
---
+++ MyPersonalWebsite/Models/LanguageCottage.cs
@@ -0,0 +1,13 @@
+﻿namespace MyPersonalWebsite.Models
+{
+    public class LanguageCottage
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? Romaji { get; set; }
+        public string? OriginalText { get; set; }
+        public string? EnglishTranslation { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
