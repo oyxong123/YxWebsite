742b4595a0564990096470840727cd38607f89e1
yuxuan <64518301+oyxong123@users.noreply.github.com>
2023-07-02 12:18:06 +0800

    create constructor for languagecottage models

--- YxWebsite/Models/JapaneseDictionary.cs
+++ YxWebsite/Models/JapaneseDictionary.cs
@@ -2,11 +2,28 @@
 {
     public class JapaneseDictionary
     {
+        // Class constructor.
+        public JapaneseDictionary
+            (
+            string phrase,
+            string pKatakana,
+            string pHiragana,
+            string romaji,
+            string meaning
+            ) 
+        { 
+            Phrase = phrase;
+            PronunciationKatakana = pKatakana;
+            PronunciationHiragana = pHiragana;
+            Romaji = romaji;
+            Meaning = meaning;
+        }
+
         public int Id { get; set; }
-        public string? Phrase { get; set; }
-        public string? PronunciationKatakana { get; set; }
-        public string? PronunciationHiragana { get; set; }
-        public string? Romaji { get; set; }
-        public string? Meaning { get; set; }
+        public required string Phrase { get; set; }
+        public required string PronunciationKatakana { get; set; }
+        public required string PronunciationHiragana { get; set; }
+        public required string Romaji { get; set; }
+        public required string Meaning { get; set; }
     }
 }
--- YxWebsite/Models/LanguageCottage.cs
+++ YxWebsite/Models/LanguageCottage.cs
@@ -1,13 +1,33 @@
-﻿namespace MyPersonalWebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPersonalWebsite.Models
 {
     public class LanguageCottage
     {
+        // Constructor.
+        public LanguageCottage
+            (
+            string romaji,
+            string originalText,
+            string englishTranslation,
+            byte[] image
+            )
+        {
+            Romaji = romaji;
+            OriginalText = originalText;
+            EnglishTranslation = englishTranslation;
+            Image = image;
+            AddedDateTime = DateTime.Now;
+            LastModifiedDateTime = DateTime.Now;
+        }
+
+        [Key]
         public int Id { get; set; }
-        public string? Romaji { get; set; }
-        public string? OriginalText { get; set; }
-        public string? EnglishTranslation { get; set; }
-        public byte[]? Image { get; set; }
-        public DateTime AddedDateTime { get; set; }
-        public DateTime LastModifiedDateTime { get; set; }
+        public required string Romaji { get; set; }
+        public required string OriginalText { get; set; }
+        public required string EnglishTranslation { get; set; }
+        public required byte[] Image { get; set; }
+        public required DateTime AddedDateTime { get; set; }
+        public required DateTime LastModifiedDateTime { get; set; }
     }
 }
