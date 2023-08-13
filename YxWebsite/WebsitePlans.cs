9c97234590be0dec69040ed82ec0db3f36abcf63
yuxuan <xuanyuong@gmail.com>
2023-08-13 15:38:58 +0800

    Partially created lc dialog.

--- YxWebsite/WebsitePlans.cs
+++ YxWebsite/WebsitePlans.cs
@@ -4,10 +4,11 @@ Excited Achievements:
 - 
 
 Website Plans:
-- Add a login function so that if it is me that has logged in, I can utilize the add post delete post etc function
+- Add a login function so that if it is me that has logged in, I can utilize the add post delete post etc function.
  
 
 Special Notes:
+- The dialogs of Radzen look better. Use Radzen for dialogs. 
 - 
  
 Thoughts:
