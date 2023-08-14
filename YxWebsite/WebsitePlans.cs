62e74a7fcc874842b054a223d4dc8b3fba244a26
yuxuan <xuanyuong@gmail.com>
2023-08-15 00:21:25 +0800

    Updated Lc. Coded out the algorithm to store and display image.

--- YxWebsite/WebsitePlans.cs
+++ YxWebsite/WebsitePlans.cs
@@ -6,11 +6,12 @@ Excited Achievements:
 Website Plans:
 - Continue set up the image upload dialog for lc, currently setting up the flow and image conversion of the drop zone.
 - Add a login function so that if it is me that has logged in, I can utilize the add post delete post etc function.
-- 
+- Resize image of Lc upload properly. (https://www.w3docs.com/snippets/css/how-to-auto-resize-an-image-to-fit-an-html-container.html)
  
 
 Special Notes:
 - The dialogs of Radzen look better. Use Radzen for dialogs. 
+- New and edit pages/dialog should reuse the same component/code to reduce duplicated code.
 - 
  
 Thoughts:
