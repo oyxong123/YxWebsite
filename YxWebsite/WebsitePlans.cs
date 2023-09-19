a7120781ca661002945ad052f5a1141528cfe07e
yuxuan <xuanyuong@gmail.com>
2023-09-19 08:23:03 +0800

    Styled LC page

--- YxWebsite/WebsitePlans.cs
+++ YxWebsite/WebsitePlans.cs
@@ -1,7 +1,9 @@
 ﻿/*
 
 What I Did:
-- 
+- Fixed login nav menu item not collapsing again bug.
+- Clean code. 
+- Styled LC page. 
 
 Excited Achievements:
 - Successfully work out the image storing & displaying algorithm. (and the supporting css for framing too)
@@ -9,9 +11,8 @@ Excited Achievements:
 - 
 
 Website Plans:
+- Style LC page. 
 - Add a login function so that if it is me that has logged in, I can utilize the add post delete post etc function.
-- Fill validator stringin LC dialog. 
-- Create sign up page. 
 - When small screen, display lc in one line like how it's usually documented. If desktop big screen, make picture at left side, and all other details at the right side like instagram desktop.
 - Implement LC category selection. (Have another nav menu of sorts at right or left side to show what category the user is viewing, they can switch to other categories at any time)
 - Add search feature to search for the record based on record ID in top right side of the page.
